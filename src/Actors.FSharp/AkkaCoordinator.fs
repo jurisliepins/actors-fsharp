86a42a187e45b2ddf7ac2461d64b6b91a8185791
Juris Liepins <juris.liepins@hotmail.com>
2021-12-24 15:58:21 +0000

    Typed message to get around akka.net issues

--- src/Actors.FSharp/AkkaCoordinator.fs
+++ src/Actors.FSharp/AkkaCoordinator.fs
@@ -1,8 +1,6 @@
 ﻿namespace Actors.FSharp
 
 open System
-open System.Collections
-open System.Collections.Generic
 open System.Collections.Concurrent
 open Akka.Actor
 open Akka.Util
@@ -10,12 +8,12 @@ open Akka.Util
 module AkkaCoordinator =
     open Akka.FSharp
     
-    type CoordinatorMessage<'a> =
-        | Request   of 'a
-        | Processed of 'a
+    type CoordinatorMessage =
+        | Request
+        | Processed of int
         
-    type WorkerMessage<'a> =
-        | Response of 'a option
+    type WorkerMessage =
+        | Response of int option
     
     let run () =
         let baseName = "akka-coordinator-fsharp"
@@ -33,12 +31,12 @@ module AkkaCoordinator =
         let complete = new BlockingCollection<_>(ConcurrentQueue())
         let coordinator = 
             spawnOpt system coordinatorName
-                (fun (mailbox: Actor<CoordinatorMessage<int>>) ->
+                (fun (mailbox: Actor<CoordinatorMessage>) ->
                     let rec iterate (incomplete: ConcurrentBag<_>) (processing: ConcurrentSet<_>) = actor {
                         match! mailbox.Receive() with
-                        | Request request ->
+                        | Request _ ->
 //                            logDebugf mailbox $"Request from %A{request}"
-                            match request with
+                            match incomplete with
                             | _ when incomplete.IsEmpty ->
 //                                logDebugf mailbox "Incomplete collection empty - taking from processing items"
                                 mailbox.Sender() <! Response (Seq.tryHead processing)
@@ -52,7 +50,7 @@ module AkkaCoordinator =
                                     | false ->
                                         failwith "Failed to add work to the processing collection"
                                 | _ ->
-                                    mailbox.Sender() <! Response None
+                                    failwith "Failed to take value from incomplete collection"
                         | Processed value ->
 //                            logDebugf mailbox $"%A{value} has been processed"
                             match processing.TryRemove value with
@@ -60,7 +58,7 @@ module AkkaCoordinator =
                                 incomplete.IsEmpty &&
                                 processing.IsEmpty ->
                                 complete.Add (Some value)
-                                complete.Add None 
+                                complete.Add None
                             | true when
                                 not incomplete.IsEmpty ||
                                 not processing.IsEmpty ->
@@ -70,12 +68,12 @@ module AkkaCoordinator =
                                 ()
                         return! iterate incomplete processing                       
                     }
-                    iterate (ConcurrentBag<_>([| for idx in 1..1_000 do yield idx |])) (ConcurrentSet<_>()))
+                    iterate (ConcurrentBag<_>([| for idx in 1..100 do yield idx |])) (ConcurrentSet<_>()))
                 [ SpawnOption.SupervisorStrategy (Strategy.OneForOne (fun _ -> Directive.Stop)) ]    
         let workers =
-            [for idx in 1..50 do
+            [for idx in 1..10 do
                 spawnOpt system (workerName idx)
-                    (fun (mailbox: Actor<WorkerMessage<int>>) ->
+                    (fun (mailbox: Actor<WorkerMessage>) ->
                         let rec iterate () = actor {
                             match! mailbox.Receive() with
                             | Response response ->
@@ -84,13 +82,14 @@ module AkkaCoordinator =
 //                                    logDebugf mailbox $"Processing value %A{value}"
                                     Async.Sleep (Random().Next(1000, 10_000)) |> Async.RunSynchronously
                                     coordinator <! Processed value
-                                    coordinator <! Request idx
+                                    coordinator <! Request
                                     return! iterate ()
                                 | None ->
-//                                    logDebugf mailbox "Done"
+//                                  // Terminal state - we've received poison-pill so time to shut down.
+//                                    logDebugf mailbox $"Worker done"
                                     ()
                         }
-                        iterate (coordinator <! Request idx))
+                        iterate (coordinator <! Request))
                     [ SpawnOption.SupervisorStrategy (Strategy.OneForOne (fun _ -> Directive.Stop)) ]
             ]
         let rec take () = seq {
